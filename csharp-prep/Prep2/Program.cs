f6a0720a5a78c68f567b2dbfd0fb2e0d1b25f182
Hanna Carroll <car18034@byui.edu>
2026-09-16 12:16:22 -0600

    Completed Prep 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percent? ");
+        float gradePercent = float.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (gradePercent >=90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >=80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >=70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >=60)
+        {
+            letter = "D";
+        }
+        else if (gradePercent >= 0)
+        {
+            letter = "F";
+        }
+        else if (gradePercent < 0)
+        {
+            letter = "negative. Please try again";
+        }
+
+        string sign = "";
+
+        if (!(letter == "A" || letter == "F" || gradePercent < 0) && gradePercent % 10 >= 7)
+        {
+            sign = "+";
+        }
+        else if (gradePercent >= 60 && gradePercent % 10 < 3)
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}.");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("You passed! :)");
+        }
+        else if (gradePercent >= 0)
+        {
+            Console.WriteLine("Better luck next time. :(");
+        }
     }
 }
